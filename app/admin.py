999b1f4f4fc1b0a2366d953e332be24dd34a80b7
Nikita Gordeev <nagordeev0106@yandex.ru>
2023-03-20 22:55:35 +0300

    some_changes

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(UserInfo)
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'games', 'wins', 'rating')
--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','username', 'email', 'is_superuser')
